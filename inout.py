729a65f6c7f5d20a92c668213aceee162fc434f4
Your Name <youremail@example.com>
2025-12-04 21:51:50 +0700

    always simple first complex come later on

---
+++ inout.py
@@ -0,0 +1,13 @@
+# Asking for input 
+name = input("enter your name: ")
+
+# Converting input to an integer
+age = int(input("Enter your age: "))
+
+# Outputting text
+print("Hello,", name)
+print("You are", age , "years old.")
+
+# Formatted outpu
+print(f"Next year you will be {age + 1}.")
+
